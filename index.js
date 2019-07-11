8ed6d4876d0d45560c372d047c4429a1f654bd14
Max MARIE <max.marie@atos.net>
2019-07-11 14:02:55 +0200

    pwd in file

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var fs = require('fs');
 
 var app = express();
 
+var PASSWORD = fs.readFileSync('pwd.txt');
+
 // Serve static folder
 app.use(express.static('client-react/build'));
 
 
 // Check authentication
 app.get('/auth', function(req, res) {
-    if (req.query.pwd == 'E203') {
+    if (req.query.pwd == PASSWORD) {
         res.send( { status: 'OK', token: 'lkESF7855hbnefS544SrefESue11Wm' });
     } else {
         res.send( {status: 'error' } );
