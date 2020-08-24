de2e0cf51d14b726c98d3d3bb87d39adbf3f963b
max marie <max.marie.4@gmail.com>
2020-08-24 14:07:25 +0800

    removed vuejs client

--- server-klaus.js
+++ server-klaus.js
@@ -1,174 +1,203 @@
-var express = require('express');
-var fs = require('fs');
-const {Dataset} = require('data.js');
-var KlausMath = require('./utils.js');
+var express = require("express");
+var fs = require("fs");
+const { Dataset } = require("data.js");
+var KlausMath = require("./utils.js");
 
 var app = express();
 
-const PASSWORD = fs.readFileSync('pwd.txt', 'utf8').trim();
-const leagues = [{
-  path: 'https://datahub.io/sports-data/french-ligue-1/datapackage.json',
-  name: 'Ligue1'
-}, {
-  path: 'https://datahub.io/sports-data/english-premier-league/datapackage.json',
-  name: 'PremierLeague'
-}, {
-  path: 'https://datahub.io/sports-data/german-bundesliga/datapackage.json',
-  name : 'Bundesliga' 
-}, {
-  path: 'https://datahub.io/sports-data/italian-serie-a/datapackage.json',
-  name: 'SerieA'
-}, {
-  path: 'https://datahub.io/sports-data/spanish-la-liga/datapackage.json',
-  name: 'LaLiga'
-}];
+const PASSWORD = fs.readFileSync("pwd.txt", "utf8").trim();
+const leagues = [
+  {
+    path: "https://datahub.io/sports-data/french-ligue-1/datapackage.json",
+    name: "Ligue1",
+  },
+  {
+    path:
+      "https://datahub.io/sports-data/english-premier-league/datapackage.json",
+    name: "PremierLeague",
+  },
+  {
+    path: "https://datahub.io/sports-data/german-bundesliga/datapackage.json",
+    name: "Bundesliga",
+  },
+  {
+    path: "https://datahub.io/sports-data/italian-serie-a/datapackage.json",
+    name: "SerieA",
+  },
+  {
+    path: "https://datahub.io/sports-data/spanish-la-liga/datapackage.json",
+    name: "LaLiga",
+  },
+];
 var stats = {};
 
 // --------------------------- Routes -----------------------------------------------------------------------------
 // Check authentication
-app.get('/auth', function(req, res) {
-    if (req.query.pwd == PASSWORD) {
-        launch();
-        res.send( { status: 'OK' });
-    } else {
-        res.send( {status: 'error' } );
-    }
+app.get("/auth", function (req, res) {
+  if (req.query.pwd == PASSWORD) {
+    launch();
+    res.send({ status: "OK" });
+  } else {
+    res.send({ status: "error" });
+  }
 });
 
 // Calculate probabilities for two teams
-app.get('/probabilities', function(req, res) {
-    var team1 = req.query.team1;
-    var team2 = req.query.team2;
-    console.log('Calculating probabilities for a match between', team1, 'and', team2, '...');
-    // Get stats for this team
-    if(stats[req.query.league][team1] == undefined) {
-      res.send(JSON.stringify({ missing: 'No home team in database'}))
-    } else if(stats[req.query.league][team2] == undefined) {
-      res.send(JSON.stringify({ missing: 'No away team in database'}))
-    } else {
-      var grid = KlausMath.getGrid(stats[req.query.league][team1].homeATK, stats[req.query.league][team1].homeDEF, stats[req.query.league][team2].awayATK, stats[req.query.league][team2].awayDEF);
-      console.log('Sending probabilities for a match between', team1, 'and', team2);
-      res.send(JSON.stringify(grid));
-    }
+app.get("/probabilities", function (req, res) {
+  var team1 = req.query.team1;
+  var team2 = req.query.team2;
+  console.log(
+    "Calculating probabilities for a match between",
+    team1,
+    "and",
+    team2,
+    "..."
+  );
+  // Get stats for this team
+  if (stats[req.query.league][team1] == undefined) {
+    res.send(JSON.stringify({ missing: "No home team in database" }));
+  } else if (stats[req.query.league][team2] == undefined) {
+    res.send(JSON.stringify({ missing: "No away team in database" }));
+  } else {
+    var grid = KlausMath.getGrid(
+      stats[req.query.league][team1].homeATK,
+      stats[req.query.league][team1].homeDEF,
+      stats[req.query.league][team2].awayATK,
+      stats[req.query.league][team2].awayDEF
+    );
+    console.log(
+      "Sending probabilities for a match between",
+      team1,
+      "and",
+      team2
+    );
+    res.send(JSON.stringify(grid));
+  }
 });
-
-// Display home page
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-})
-app.get('/vuejs.png', function(req, res) {
-  res.sendFile(__dirname + '/vuejs.png');
-})
-app.get('/react.png', function(req, res) {
-  res.sendFile(__dirname + '/react.png');
-})
 
 // --------------------- Functions called periodically to update dataset and statistics ---------------------------
 // Init the dataset with all games
-var initData = async function(league) {
-    const dataset = await Dataset.load(league.path)
-    for (const id in dataset.resources) {
-        console.log(dataset.resources[id]._descriptor.name)
-      }
-      // get all tabular data(if exists any)
-      for (const id in dataset.resources) {
-        if (dataset.resources[id]._descriptor.name === "season-1819_json") {
-          const writeStream = fs.createWriteStream('data/' + league.name + '-' + dataset.resources[id]._descriptor.name.replace('_', '.'));
-          const file = dataset.resources[id]
-          // Get a raw stream
-          const stream = await file.stream();
-          const buffer = await file.buffer;
-          stream.pipe(writeStream)
-        }
-      }
-}
-
+var initData = async function (league) {
+  const dataset = await Dataset.load(league.path);
+  for (const id in dataset.resources) {
+    console.log(dataset.resources[id]._descriptor.name);
+  }
+  // get all tabular data(if exists any)
+  for (const id in dataset.resources) {
+    if (dataset.resources[id]._descriptor.name === "season-1819_json") {
+      const writeStream = fs.createWriteStream(
+        "data/" +
+          league.name +
+          "-" +
+          dataset.resources[id]._descriptor.name.replace("_", ".")
+      );
+      const file = dataset.resources[id];
+      // Get a raw stream
+      const stream = await file.stream();
+      const buffer = await file.buffer;
+      stream.pipe(writeStream);
+    }
+  }
+};
 
 // Counts average goals for a league and a season, and counts avg goals for each team (when away and home)
-var countStatsFromFile = function(league, season, teams) {
-    var path = 'data/' + league.name + '-season-'+ season + '.json';
-    if(fs.existsSync(path)) {
-      var data = fs.readFileSync(path);
-      var leagueJsonObj = JSON.parse(data);
-      var leagueHomeGoals = 0;
-      var leagueAwayGoals = 0;
-      // Get avg goals for whole league and count goals per team, number of match per team, and global stats for the league (goals from home team and away team)
-      for(var j = 0; j < leagueJsonObj.length; j++) {
-        var awayTeam = leagueJsonObj[j].AwayTeam;
-        var homeTeam = leagueJsonObj[j].HomeTeam;
-        var awayGoal = leagueJsonObj[j].FTAG;
-        var homeGoal = leagueJsonObj[j].FTHG;
-        // Count global league stats
-        leagueHomeGoals += homeGoal;
-        leagueAwayGoals += awayGoal;
-        // Count goals for homeTeam
-        if (teams.hasOwnProperty(homeTeam)) {
-          teams[homeTeam].goalsScoredHome += homeGoal;
-          teams[homeTeam].goalsTakenHome += awayGoal;
-          teams[homeTeam].nbMatchHome++;
-        } else {
-          teams[homeTeam] = { goalsScoredHome: homeGoal, goalsTakenHome: awayGoal, nbMatchHome: 1 , goalsScoredAway: 0, goalsTakenAway: 0, nbMatchAway: 0 };
-        }
-        // Count goals for awayTeam
-        if (teams.hasOwnProperty(awayTeam)) {
-          teams[awayTeam].goalsScoredAway += awayGoal;
-          teams[awayTeam].goalsTakenAway += homeGoal;
-          teams[awayTeam].nbMatchAway++;
-        } else {
-          teams[awayTeam] = { goalsScoredAway: awayGoal, goalsTakenAway: homeGoal, nbMatchAway: 1 , goalsScoredHome: 0, goalsTakenHome: 0, nbMatchHome: 0 };
-        }
+var countStatsFromFile = function (league, season, teams) {
+  var path = "data/" + league.name + "-season-" + season + ".json";
+  if (fs.existsSync(path)) {
+    var data = fs.readFileSync(path);
+    var leagueJsonObj = JSON.parse(data);
+    var leagueHomeGoals = 0;
+    var leagueAwayGoals = 0;
+    // Get avg goals for whole league and count goals per team, number of match per team, and global stats for the league (goals from home team and away team)
+    for (var j = 0; j < leagueJsonObj.length; j++) {
+      var awayTeam = leagueJsonObj[j].AwayTeam;
+      var homeTeam = leagueJsonObj[j].HomeTeam;
+      var awayGoal = leagueJsonObj[j].FTAG;
+      var homeGoal = leagueJsonObj[j].FTHG;
+      // Count global league stats
+      leagueHomeGoals += homeGoal;
+      leagueAwayGoals += awayGoal;
+      // Count goals for homeTeam
+      if (teams.hasOwnProperty(homeTeam)) {
+        teams[homeTeam].goalsScoredHome += homeGoal;
+        teams[homeTeam].goalsTakenHome += awayGoal;
+        teams[homeTeam].nbMatchHome++;
+      } else {
+        teams[homeTeam] = {
+          goalsScoredHome: homeGoal,
+          goalsTakenHome: awayGoal,
+          nbMatchHome: 1,
+          goalsScoredAway: 0,
+          goalsTakenAway: 0,
+          nbMatchAway: 0,
+        };
       }
-      var leagueAvgHomeGoals = leagueHomeGoals / leagueJsonObj.length;
-      var leagueAvgAwayGoals = leagueAwayGoals / leagueJsonObj.length;
-      // Do averaging
-      for(name in teams) {
-        var team = teams[name];
-        //team.homeATK = (team.goalsScoredHome / team.nbMatchHome) * leagueAvgHomeGoals;
-        //team.awayATK = (team.goalsScoredAway / team.nbMatchAway) * leagueAvgAwayGoals;
-        team.homeATK = (team.goalsScoredHome / team.nbMatchHome);
-        team.awayATK = (team.goalsScoredAway / team.nbMatchAway);
-        team.homeDEF = team.goalsTakenHome / team.nbMatchHome;
-        team.awayDEF = team.goalsTakenAway / team.nbMatchAway;
+      // Count goals for awayTeam
+      if (teams.hasOwnProperty(awayTeam)) {
+        teams[awayTeam].goalsScoredAway += awayGoal;
+        teams[awayTeam].goalsTakenAway += homeGoal;
+        teams[awayTeam].nbMatchAway++;
+      } else {
+        teams[awayTeam] = {
+          goalsScoredAway: awayGoal,
+          goalsTakenAway: homeGoal,
+          nbMatchAway: 1,
+          goalsScoredHome: 0,
+          goalsTakenHome: 0,
+          nbMatchHome: 0,
+        };
       }
     }
-    return teams;
-}
+    var leagueAvgHomeGoals = leagueHomeGoals / leagueJsonObj.length;
+    var leagueAvgAwayGoals = leagueAwayGoals / leagueJsonObj.length;
+    // Do averaging
+    for (name in teams) {
+      var team = teams[name];
+      //team.homeATK = (team.goalsScoredHome / team.nbMatchHome) * leagueAvgHomeGoals;
+      //team.awayATK = (team.goalsScoredAway / team.nbMatchAway) * leagueAvgAwayGoals;
+      team.homeATK = team.goalsScoredHome / team.nbMatchHome;
+      team.awayATK = team.goalsScoredAway / team.nbMatchAway;
+      team.homeDEF = team.goalsTakenHome / team.nbMatchHome;
+      team.awayDEF = team.goalsTakenAway / team.nbMatchAway;
+    }
+  }
+  return teams;
+};
 
 // Calculate the ATK and DEF for all teams
-var updateAtkDef = function(league) {
-    console.log('[', league.name, '] : Calculate ATK and DEF...');
-    
-    var teams1819 = countStatsFromFile(league, '1819', {});
-    var teams1920 = countStatsFromFile(league, '1920', teams1819);
-    
-    return teams1920;
-}
+var updateAtkDef = function (league) {
+  console.log("[", league.name, "] : Calculate ATK and DEF...");
 
-var initAll = function() {
-  leagues.forEach(league => {
+  var teams1819 = countStatsFromFile(league, "1819", {});
+  var teams1920 = countStatsFromFile(league, "1920", teams1819);
+
+  return teams1920;
+};
+
+var initAll = function () {
+  leagues.forEach((league) => {
     stats[league.name] = initData(league);
   });
-}
+};
 
-var computeStatsFromFiles = function() {
+var computeStatsFromFiles = function () {
   var new_stats = {};
-  leagues.forEach(league => {
+  leagues.forEach((league) => {
     new_stats[league.name] = updateAtkDef(league);
-  })
+  });
   return new_stats;
-}
+};
 
 // Serve static folder and listen
-var launch = function() {
+var launch = function () {
   // Get the data
   initAll();
   // Update stats from data
-  setTimeout(function() {
-      stats = computeStatsFromFiles(leagues);
-    } , 10000);
-}
+  setTimeout(function () {
+    stats = computeStatsFromFiles(leagues);
+  }, 10000);
+};
 
 launch();
-app.use('/client-react', express.static('client-react/build'));
-app.use('/client-vuejs', express.static('client-vuejs'));
+app.use("/client-react", express.static("client-react/build"));
 app.listen(80);
